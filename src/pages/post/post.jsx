741999d2e1c7eb3c5a7ace0cff5678bc3cbee9cc
murph <murphyl@outlook.com>
2020-05-25 16:32:24 +0800

    移除代码高亮部分

--- src/pages/post/post.jsx
+++ src/pages/post/post.jsx
@@ -1,40 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from "react-router-dom";
-import parseFrontMatter from 'frontmatter';
 import Markdown from 'markdown-to-jsx';
-
-// highlight.js
-import hljs from 'highlight.js';
-import 'highlight.js/styles/github.css';
 
 import { ajaxGet } from 'utils/rest_client';
 
 import './post.css';
-
-const LANG_TYPES = {
-    'lang-sh': 'Shell'
-};
 
 const Title = ({ type, children }) => {
     return React.createElement(type, { className: 'title' }, children);
 }
 
 const H3 = (props) => (<Title type='h3' { ...props } />);
-
-const Prepare = ({ children }) => {
-    if(children && children.type === 'code') {
-        const langType = LANG_TYPES[children.props.className] || 'Text';
-        return (
-            <div className="code-block">
-                <pre>{ children }</pre>
-                <div className="lang-type">{ langType }</div>
-            </div>
-        )
-    }
-    return (
-        <div>TODO prepare block</div>
-    );
-};
 
 const Paragraph = ({ children }) => {
     if(children && Array.isArray(children)) {
@@ -69,9 +45,6 @@ const markdownOptions = {
         h6: {
             component:  (props) => (<Title type='h6' { ...props } />)
         },
-        p: {
-            component: Paragraph
-        },
         pre: {
             component: Prepare
         },
@@ -91,16 +64,6 @@ const markdownOptions = {
     }
 };
 
-const highlightCodeBlock = () => {
-    hljs.configure({
-        tabReplace: '    ',
-    });
-    hljs.initHighlighting();
-    document.querySelectorAll('.code-block code').forEach((block) => {
-        hljs.highlightBlock(block);
-    });    
-}
-
 function Post() {
     const { unique } = useParams();
     const [ post, setPost ] = useState({});
@@ -111,10 +74,7 @@ function Post() {
             } else {
                 return { code: 1 }
             }
-        }).then((result) => {
-            setPost(result);
-            setTimeout(highlightCodeBlock, 50);
-        })
+        }).then(setPost)
     }, [ unique ]);
     return (
         <article>
